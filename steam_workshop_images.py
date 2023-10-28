07c51ed47796d0f0df6277dccf3668e635b58428
copyrite <tatu.heikki.leinonen@gmail.com>
2023-10-28 12:36:25 +0300

    Add Mob / Aim frequency matrix

--- steam_workshop_images.py
+++ steam_workshop_images.py
@@ -3,8 +3,9 @@ from textwrap import dedent
 
 import numpy as np
 import matplotlib as mpl
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-import matplotlib.patches as patch
+import matplotlib.ticker as mtick
 
 from soldier import Soldier, LWOTCSoldier, ActuallyNCESoldier
 
@@ -17,6 +18,15 @@ EXPLAINER = {
     "alpha": 0.5,
 }
 IMG_FILE_PREFIX = "SteamWorkshop_"
+
+AIM_RANGE = range(
+    Soldier.STATS["Offense"].default + Soldier.STATS["Offense"].min_delta,
+    Soldier.STATS["Offense"].default + Soldier.STATS["Offense"].max_delta + 1,
+)
+MOB_RANGE = range(
+    Soldier.STATS["Mobility"].default + Soldier.STATS["Mobility"].min_delta,
+    Soldier.STATS["Mobility"].default + Soldier.STATS["Mobility"].max_delta + 1,
+)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("python steam_workshop_images.py")
@@ -42,16 +52,26 @@ if __name__ == "__main__":
 
     totals_fig, totals_ax = plt.subplots()
     corr_fig, corr_axes = plt.subplots(2)
+    mob_aim_fig, mob_aim_axes = plt.subplot_mosaic(
+        [["top", "colorbar"], ["bottom", "colorbar"]], width_ratios=[95, 5]
+    )
+    mob_aim_samples = []  # Need to plot after loop, otherwise colormap won't be shared
 
     for sample_index, factory in enumerate((LWOTCSoldier, ActuallyNCESoldier)):
         # Put sample in a matrix
         sample = np.zeros([args.number, len(Soldier.STATS)], dtype=np.int16)
         totals = np.zeros([args.number], dtype=np.int16)
         default_total = Soldier().weighed_stat_total()
+        mob_aim_sample = np.zeros([len(MOB_RANGE), len(AIM_RANGE)], dtype=np.uint64)
+
         for i in range(args.number):
             sol = factory()
             sample[i, :] = [getattr(sol, stat).current for stat in Soldier.STATS]
             totals[i] = sol.weighed_stat_total() - default_total
+            mob_aim_sample[
+                getattr(sol, "Mobility").current - min(MOB_RANGE),
+                getattr(sol, "Offense").current - min(AIM_RANGE),
+            ] += 1
 
         # The 7 stat charts
         for stat_index, (stat, range_) in enumerate(Soldier.STATS.items()):
@@ -76,7 +96,7 @@ if __name__ == "__main__":
                 linewidth=0.75,
             )
 
-        # Covariance matrices & Aim/Mob correlation chart
+        # Covariance matrices
         cov = np.cov(sample.T)
         cov /= np.sqrt(np.asmatrix(cov).diagonal().T * np.asmatrix(cov).diagonal())
         corr_axes[sample_index].pcolor(
@@ -85,6 +105,9 @@ if __name__ == "__main__":
             vmin=-max(abs(cov.min(None)), abs(cov.max(None))),
             vmax=max(abs(cov.min(None)), abs(cov.max(None))),
         )
+
+        # Aim/Mob correlation chart
+        mob_aim_samples.append(mob_aim_sample)
 
     # Edit axes, save figs
     for stat in Soldier.STATS:
@@ -178,3 +201,38 @@ if __name__ == "__main__":
         patch.set_color(color)
 
     corr_fig.savefig(f"{IMG_FILE_PREFIX}corr.png")
+
+    mob_aim_max = max(sample.max(None) for sample in mob_aim_samples)
+    for ax, sample in zip(
+        [mob_aim_axes["top"], mob_aim_axes["bottom"]], mob_aim_samples
+    ):
+        ax.pcolor(sample, vmax=mob_aim_max)
+
+        aim_tick_range = range(AIM_RANGE.start, AIM_RANGE.stop, 5)
+        ax.set_xticks(
+            [0.5 + 5 * i for i in range(len(aim_tick_range))],
+            labels=aim_tick_range,
+        )
+        ax.set_yticks(
+            [0.5 + i for i in range(len(MOB_RANGE))],
+            labels=MOB_RANGE,
+        )
+        ax.set_xlabel("Offense")
+        ax.set_ylabel("Mobility")
+    mob_aim_axes["top"].set_title(f"Base LWOTC (n = {args.number})")
+    mob_aim_axes["bottom"].set_title(f"Actually NCE (n = {args.number})")
+
+    mob_aim_axes["colorbar"].set_axis_off()
+    colorbar = mob_aim_fig.colorbar(
+        cm.ScalarMappable(),
+        ax=mob_aim_axes["colorbar"],
+        orientation="vertical",
+        location="right",
+        fraction=1,
+    )
+    colorbar.set_ticks(
+        [i / 5 for i in range(6)],
+        labels=[f"{100 * i * mob_aim_max / 5 / args.number}%" for i in range(6)],
+    )
+    mob_aim_fig.tight_layout()
+    mob_aim_fig.savefig(f"{IMG_FILE_PREFIX}Mob_Aim.png")
